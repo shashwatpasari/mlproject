1f9e451f022cef35dee205ef3e8aab72bd8c85dd
shashwat-airveda <shashwat.pasari@airveda.com>
2023-07-30 00:21:10 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -27,7 +27,23 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info('Train test split initiated')
-            train_set,test_set=train_test_split(df,test_size=0.2)
+            train_set,test_set=train_test_split(df,test_size=0.2, random_state=42)
 
-        except:
-            pass
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
